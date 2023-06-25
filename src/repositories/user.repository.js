5fd947fa68f0d4b309b19bb48ab64448a4dc5fc8
Felipe Ulloa <felipeulloa5a43@gmail.com>
2023-06-24 20:13:23 -0400

    fix: Que retorne true solo si creo o elimino un nuevo usuario

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -19,7 +19,11 @@ class UserRepository {
         "INSERT INTO usuario(rut, correo, contrasena, carrera, nombre, ano_ingreso, es_admin) VALUES($1, $2, $3, $4, $5, $6, $7)",
         [rut, correo, contrasena, carrera, nombre, ano_ingreso, es_admin]
       );
-      return true;
+      const result = await this.pool.query(
+        "SELECT * FROM usuario WHERE rut = $1",
+        [rut]
+      );
+      return result.rows.length > 0 ? true : false;
     } catch (error) {
       console.error(error);
       return false;
@@ -28,11 +32,11 @@ class UserRepository {
 
   async changePassword(contrasena, correo) {
     try {
-      await this.pool.query(
+      const result = await this.pool.query(
         'UPDATE "usuario" SET contrasena=$1 WHERE correo = $2',
         [contrasena, correo]
       );
-      return true;
+      return result.rowCount > 0 ? true : false;
     } catch (error) {
       console.error(error);
       return false;
@@ -41,8 +45,11 @@ class UserRepository {
 
   async deleteUser(rut) {
     try {
-      await this.pool.query("DELETE FROM usuario WHERE rut = $1", [rut]);
-      return true;
+      const result = await this.pool.query(
+        "DELETE FROM usuario WHERE rut = $1",
+        [rut]
+      );
+      return result.rowCount > 0 ? true : false;
     } catch (error) {
       console.error(error);
       return false;
