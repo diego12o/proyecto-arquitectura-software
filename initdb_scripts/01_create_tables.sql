c43e36f94e17d8ce1bc304474cd3d6472472d3eb
Felipe Ulloa <felipeulloa5a43@gmail.com>
2023-06-26 00:39:22 -0400

    chore: agregar relacion entre evaluacion y usuario

--- initdb_scripts/01_create_tables.sql
+++ initdb_scripts/01_create_tables.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS profesor (
 
 CREATE TABLE IF NOT EXISTS profesor_curso (
   id SERIAL PRIMARY KEY,
-  codigo_curso varchar(50) NOT NULL,
+  codigo_curso int NOT NULL,
   id_profesor int NOT NULL,
   FOREIGN KEY (codigo_curso) REFERENCES curso (codigo),
   FOREIGN KEY (id_profesor) REFERENCES profesor (id)
@@ -20,11 +20,13 @@ CREATE TABLE IF NOT EXISTS profesor_curso (
 
 CREATE TABLE IF NOT EXISTS evaluacion (
   id SERIAL PRIMARY KEY,
+  rut_usuario int NOT NULL,
   id_profesor_curso int NOT NULL,
   comentario text NOT NULL,
   nota real NOT NULL DEFAULT '1',
   fecha timestamp NOT NULL,
-  FOREIGN KEY (id_profesor_curso) REFERENCES profesor_curso (id)
+  FOREIGN KEY (id_profesor_curso) REFERENCES profesor_curso (id),
+  FOREIGN KEY (rut_usuario) REFERENCES usuario (rut)
 );
 
 CREATE TABLE IF NOT EXISTS usuario (
